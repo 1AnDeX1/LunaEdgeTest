63371cd794105be565d705b2bd9dac941366f2a1
1AnDeX1 <akhrobatenko56@gmail.com>
2024-09-07 19:33:16 +0300

    added some validation

--- TestAssignment/TestAssignment.BLL/Models/UpdateTaskDto.cs
+++ TestAssignment/TestAssignment.BLL/Models/UpdateTaskDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -10,10 +11,22 @@ namespace TestAssignment.BLL.Models
 {
     public class UpdateTaskDto
     {
+        [Required(ErrorMessage = "The title is required.")]
+        [StringLength(100, ErrorMessage = "The title cannot be longer than 100 characters.")]
         public required string Title { get; set; }
+
+        [StringLength(500, ErrorMessage = "The description cannot be longer than 500 characters.")]
         public string? Description { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Invalid date format.")]
         public DateTime? DueDate { get; set; }
+
+        [Required(ErrorMessage = "The status is required.")]
+        [EnumDataType(typeof(StatusEnum), ErrorMessage = "Invalid status value.")]
         public StatusEnum Status { get; set; }
+
+        [Required(ErrorMessage = "The priority is required.")]
+        [EnumDataType(typeof(PriorityEnum), ErrorMessage = "Invalid priority value.")]
         public PriorityEnum Priority { get; set; }
     }
 }
